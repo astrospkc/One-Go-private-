4753053ec45abafed588eb4d2478e5d1c14e4ede
Punam <punamkumari399@gmail.com>
2026-01-25 01:32:28 +0530

    redesigned signin signup

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,21 +6,7 @@ export default function AuthLayout({
     return (
         <>
             <div className="min-h-screen flex items-center justify-center relative w-full">
-                <div
-                    style={{
-                        backgroundImage: 'url("/image/miclaptop.jpg")',
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        backgroundRepeat: "no-repeat, repeat",
-                        // backgroundColor: "rgb(34, 34, 34)",
-                        backgroundColor: "black",
-                        backgroundBlendMode: "",
 
-                        opacity: 0.3, // control background image opacity here
-                        zIndex: 0,
-                    }}
-                    className="absolute inset-0"
-                ></div>
                 {children}
             </div>
         </>
